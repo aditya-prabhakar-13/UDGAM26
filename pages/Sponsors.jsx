9fbee6320cb7a95248382f8daa0bf84360f75ecd
Aditya_Prabhakar <professor130405@gmail.com>
2026-01-20 23:38:15 +0530

    Sponsors Batch-2 Added

--- pages/Sponsors.jsx
+++ pages/Sponsors.jsx
@@ -8,6 +8,22 @@ import ekara from '/ekara.svg';
 import easetrip from '/easetrip.svg';
 import razor from '/razorpay.svg';   
 import tpf from '/tpf.svg'; 
+import sharechat from '/sharechat.svg';
+import tic from '/tic.svg';
+import nrl from '/nrl.svg';
+import big_mind from '/big_mind.svg';
+import nehu from '/nehu.svg';
+import oil from '/oil.svg';
+import idp from '/idp.svg';
+import ims from '/ims.svg';
+import icfdr from '/icfdr.svg';
+import saturo from '/saturo.svg';
+import nedfi from '/nedfi.svg';
+import thesys from '/thesys.svg';
+import fof from '/fof.svg';
+import gofly from '/gofly.svg';
+import titan from '/titan.svg';
+import chirtae from '/chirtae.svg';
 
 
 
@@ -22,7 +38,7 @@ export default function Sponsors() {
             </div>
 
             <div id="title-sponsors-container" className="flex flex-col justify-center items-center mt-[1.778vw] gap-y-[3.546vw]">
-                <div id="title1col" className="w-[21.765vw] h-[14.466vw] rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6]">
+                <div id="title1col" style={{backgroundImage: `url(${slice})`}} className="w-[21.765vw] h-[14.466vw] rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] bg-cover bg-center bg-no-repeat">
 
                 </div>
                 <div id="titlecol2" className="flex flex-row items-center justify-center gap-x-[7.564vw]">
@@ -30,26 +46,27 @@ export default function Sponsors() {
                         <div id="titlecol2-card1" style={{backgroundImage: `url(${aisquare})`}} className="w-[21.633vw] h-[15.058vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] bg-cover bg-center bg-no-repeat">
                             {/* <img src={aisquare} alt="ai-square" className="object-cover object-center" /> */}
                         </div>
-                        <div id="titlecol2-card1-name" className="w-[21.633vw] h-[4.866vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6]">
-
+                        <div id="titlecol2-card1-name" className="w-[21.633vw] h-[4.866vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] flex flex-col justify-center items-center">
+                            Sparkle Title Sponsor
                         </div>
                     </div>
                     <div id="titlecol2-card-container2" className="flex flex-col justify-center items-center">
-                        <div id="titlecol2-card2" style={{backgroundImage: `url(${groundzero})`}} className="w-[21.633vw] h-[15.058vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] bg-cover bg-center bg-no-repeat">
+                        <div id="titlecol2-card2" style={{backgroundImage: `url(${sharechat})`}} className="w-[21.633vw] h-[15.058vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] bg-cover bg-center bg-no-repeat">
 
                         </div>
-                        <div id="titlecol2-card2-name" className="w-[21.633vw] h-[4.866vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6]">
-
+                        <div id="titlecol2-card2-name" className="w-[21.633vw] h-[4.866vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] flex flex-col justify-center items-center">
+                            PMx Title Sponsor
                         </div>
                     </div>
                     <div id="titlecol2-card-container3" className="flex flex-col justify-center items-center">
-                        <div id="titlecol2-card3" style={{backgroundImage: `url(${slice})`}} className="w-[21.633vw] h-[15.058vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] bg-cover bg-center bg-no-repeat">
+                        <div id="titlecol2-card3" style={{backgroundImage: `url(${groundzero})`}} className="w-[21.633vw] h-[15.058vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] bg-cover bg-center bg-no-repeat">
 
                         </div>
-                        <div id="titlecol2-card3-name" className="w-[21.633vw] h-[4.866vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6]">
-
+                        <div id="titlecol2-card3-name" className="w-[21.633vw] h-[4.866vw] shrink-0 rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] flex flex-col justify-center items-center">
+                            PMx Associate Title Sponsor
                         </div>
                     </div>
+                    
                 </div>
             </div>
 
@@ -86,31 +103,31 @@ export default function Sponsors() {
                                 <path d="M32.5693 0C32.5787 4.24367 33.4188 8.44514 35.043 12.3662C36.6762 16.309 39.0702 19.8914 42.0879 22.9092C45.1058 25.9271 48.6887 28.3218 52.6318 29.9551C56.5533 31.5794 60.7549 32.4186 64.999 32.4277C64.9991 32.4518 65 32.4759 65 32.5C65 32.5237 64.9991 32.5476 64.999 32.5713C60.7549 32.5805 56.5533 33.4206 52.6318 35.0449C48.6888 36.6782 45.1058 39.072 42.0879 42.0898C39.07 45.1077 36.6763 48.6908 35.043 52.6338C33.4665 56.4396 32.6306 60.5096 32.5742 64.626C32.4941 60.5232 31.5127 56.2693 29.9482 52.4922C28.315 48.5492 25.9211 44.9661 22.9033 41.9482C19.8855 38.9304 16.3023 36.5366 12.3594 34.9033C8.44035 33.28 4.2414 32.4389 0 32.4287V32.4277C4.2418 32.4177 8.44096 31.5785 12.3604 29.9551C16.3035 28.3218 19.8864 25.9271 22.9043 22.9092C25.922 19.8914 28.316 16.309 29.9492 12.3662C31.5733 8.44544 32.4134 4.24431 32.4229 0.000976562C32.4485 0.000917295 32.4744 0 32.5 0C32.5231 2.12026e-07 32.5462 -4.84609e-05 32.5693 0Z" fill="#343333"/>
                             </svg>
                         </div>
-                        <div id="g1-r2-c3" style={{backgroundImage: `url(${easetrip})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[42.083vw] h-[14.466vw]  bg-cover bg-center bg-no-repeat">
+                        <div id="g1-r2-c3" style={{backgroundImage: `url(${easetrip})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[42.083vw] h-[14.466vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                     </div>
                     <div id="g1-r3" className="flex flex-col items-center justify-center">
-                        <div id="g1-r3-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[14.992vw]">
+                        <div id="g1-r3-c1" style={{backgroundImage: `url(${nrl})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[14.992vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
-                        <div id="g1-r3-c2" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[18.609vw]">
+                        <div id="g1-r3-c2" style={{backgroundImage: `url(${big_mind})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[18.609vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                     </div>
                 </div>
                 <div id="partnersgroup2" className="flex flex-row items-center justify-center">
-                    <div id="g2-r1-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.781vw] h-[24.724vw]">
+                    <div id="g2-r1-c1" style={{backgroundImage: `url(${oil})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.781vw] h-[24.724vw] bg-cover bg-center bg-no-repeat">
 
                     </div>
                     <div id="g2-r2" className="flex flex-col items-center justify-center relative">
-                        <div id="g2-r2-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[19.726vw] h-[14.466vw]">
+                        <div id="g2-r2-c1" style={{backgroundImage: `url(${tic})`}}  className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[19.726vw] h-[14.466vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65 65" fill="none" className="absolute z-1 w-[4.274vw] h-[4.249vw] left-[-2.15vw] bottom-[8.1vw]">
                             <path d="M32.5693 0C32.5787 4.24367 33.4188 8.44514 35.043 12.3662C36.6762 16.309 39.0702 19.8914 42.0879 22.9092C45.1058 25.9271 48.6887 28.3218 52.6318 29.9551C56.5533 31.5794 60.7549 32.4186 64.999 32.4277C64.9991 32.4518 65 32.4759 65 32.5C65 32.5237 64.9991 32.5476 64.999 32.5713C60.7549 32.5805 56.5533 33.4206 52.6318 35.0449C48.6888 36.6782 45.1058 39.072 42.0879 42.0898C39.07 45.1077 36.6763 48.6908 35.043 52.6338C33.4665 56.4396 32.6306 60.5096 32.5742 64.626C32.4941 60.5232 31.5127 56.2693 29.9482 52.4922C28.315 48.5492 25.9211 44.9661 22.9033 41.9482C19.8855 38.9304 16.3023 36.5366 12.3594 34.9033C8.44035 33.28 4.2414 32.4389 0 32.4287V32.4277C4.2418 32.4177 8.44096 31.5785 12.3604 29.9551C16.3035 28.3218 19.8864 25.9271 22.9043 22.9092C25.922 19.8914 28.316 16.309 29.9492 12.3662C31.5733 8.44544 32.4134 4.24431 32.4229 0.000976562C32.4485 0.000917295 32.4744 0 32.5 0C32.5231 2.12026e-07 32.5462 -4.84609e-05 32.5693 0Z" fill="#343333"/>
                         </svg>
-                        <div id="g2-r2-c2" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[19.726vw] h-[10.258vw]">
+                        <div id="g2-r2-c2" style={{backgroundImage: `url(${ims})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[19.726vw] h-[10.258vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65 65" fill="none" className="absolute z-1 w-[4.274vw] h-[4.249vw] right-[-2.15vw] top-[-2.1vw]">
@@ -118,17 +135,17 @@ export default function Sponsors() {
                         </svg>
                         
                 </div>
-                    <div id="g2-r3-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[23.08vw] h-[24.724vw]">
+                    <div id="g2-r3-c1" style={{backgroundImage: `url(${icfdr})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[23.08vw] h-[24.724vw] bg-cover bg-center bg-no-repeat">
 
                     </div>
                     <div id="g2-r4" className="flex flex-col justify-center items-center relative">
-                        <div id="g2-r4-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[23.08vw] h-[14.466vw]">
+                        <div id="g2-r4-c1" style={{backgroundImage: `url(${nehu})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[23.08vw] h-[14.466vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65 65" fill="none" className="absolute z-1 w-[4.274vw] h-[4.249vw] left-[-2.15vw] bottom-[8.1vw]">
                             <path d="M32.5693 0C32.5787 4.24367 33.4188 8.44514 35.043 12.3662C36.6762 16.309 39.0702 19.8914 42.0879 22.9092C45.1058 25.9271 48.6887 28.3218 52.6318 29.9551C56.5533 31.5794 60.7549 32.4186 64.999 32.4277C64.9991 32.4518 65 32.4759 65 32.5C65 32.5237 64.9991 32.5476 64.999 32.5713C60.7549 32.5805 56.5533 33.4206 52.6318 35.0449C48.6888 36.6782 45.1058 39.072 42.0879 42.0898C39.07 45.1077 36.6763 48.6908 35.043 52.6338C33.4665 56.4396 32.6306 60.5096 32.5742 64.626C32.4941 60.5232 31.5127 56.2693 29.9482 52.4922C28.315 48.5492 25.9211 44.9661 22.9033 41.9482C19.8855 38.9304 16.3023 36.5366 12.3594 34.9033C8.44035 33.28 4.2414 32.4389 0 32.4287V32.4277C4.2418 32.4177 8.44096 31.5785 12.3604 29.9551C16.3035 28.3218 19.8864 25.9271 22.9043 22.9092C25.922 19.8914 28.316 16.309 29.9492 12.3662C31.5733 8.44544 32.4134 4.24431 32.4229 0.000976562C32.4485 0.000917295 32.4744 0 32.5 0C32.5231 2.12026e-07 32.5462 -4.84609e-05 32.5693 0Z" fill="#343333"/>
                         </svg>
-                        <div id="g2-r4-c2" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[23.08vw] h-[10.258vw]">
+                        <div id="g2-r4-c2" style={{backgroundImage: `url(${idp})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[23.08vw] h-[10.258vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65 65" fill="none" className="absolute z-1 w-[4.274vw] h-[4.249vw] left-[5.43vw] top-[-2.1vw]">
@@ -138,34 +155,34 @@ export default function Sponsors() {
                 </div>
                 <div id="partnersgroup3" className="flex flex-row items-center justify-center">
                     <div id="g4-r1" className="flex flex-col items-center justify-center">
-                        <div id="g4-r1-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[24.066vw] h-[13.677vw]">
+                        <div id="g4-r1-c1" style={{backgroundImage: `url(${thesys})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[24.066vw] h-[13.677vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
-                        <div id="g4-r1-c2" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[24.066vw] h-[19.924vw]">
+                        <div id="g4-r1-c2" style={{backgroundImage: `url(${gofly})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[24.066vw] h-[19.924vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                     </div>
                     <div id="g4-r2" className="flex flex-col items-center justify-center">
                         <div id="g4-r2-c12-container" className="flex flex-row items-center justify-center relative">
-                            <div id="g4-r2-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[16.636vw] h-[19.135vw]">
+                            <div id="g4-r2-c1" style={{backgroundImage: `url(${nedfi})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[16.636vw] h-[19.135vw] bg-cover bg-center bg-no-repeat">
 
                             </div>
-                            <div id="g4-r2-c2" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[25.447vw] h-[19.135vw]">
+                            <div id="g4-r2-c2" style={{backgroundImage: `url(${fof})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[25.447vw] h-[19.135vw] bg-cover bg-center bg-no-repeat">
 
                             </div>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65 65" fill="none" className="absolute z-1 w-[4.274vw] h-[4.249vw] left-[14.5vw] bottom-[-2.1vw]">
                                 <path d="M32.5693 0C32.5787 4.24367 33.4188 8.44514 35.043 12.3662C36.6762 16.309 39.0702 19.8914 42.0879 22.9092C45.1058 25.9271 48.6887 28.3218 52.6318 29.9551C56.5533 31.5794 60.7549 32.4186 64.999 32.4277C64.9991 32.4518 65 32.4759 65 32.5C65 32.5237 64.9991 32.5476 64.999 32.5713C60.7549 32.5805 56.5533 33.4206 52.6318 35.0449C48.6888 36.6782 45.1058 39.072 42.0879 42.0898C39.07 45.1077 36.6763 48.6908 35.043 52.6338C33.4665 56.4396 32.6306 60.5096 32.5742 64.626C32.4941 60.5232 31.5127 56.2693 29.9482 52.4922C28.315 48.5492 25.9211 44.9661 22.9033 41.9482C19.8855 38.9304 16.3023 36.5366 12.3594 34.9033C8.44035 33.28 4.2414 32.4389 0 32.4287V32.4277C4.2418 32.4177 8.44096 31.5785 12.3604 29.9551C16.3035 28.3218 19.8864 25.9271 22.9043 22.9092C25.922 19.8914 28.316 16.309 29.9492 12.3662C31.5733 8.44544 32.4134 4.24431 32.4229 0.000976562C32.4485 0.000917295 32.4744 0 32.5 0C32.5231 2.12026e-07 32.5462 -4.84609e-05 32.5693 0Z" fill="#343333"/>
                             </svg>
                         </div>
-                        <div id="g4-r2-c3" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[42.083vw] h-[14.466vw]">
+                        <div id="g4-r2-c3" style={{backgroundImage: `url(${saturo})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[42.083vw] h-[14.466vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                     </div>
                     <div id="g4-r3" className="flex flex-col items-center justify-center">
-                        <div id="g4-r3-c1" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[14.992vw]">
+                        <div id="g4-r3-c1" style={{backgroundImage: `url(${titan})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[14.992vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
-                        <div id="g4-r3-c2" className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[18.609vw]">
+                        <div id="g4-r3-c2" style={{backgroundImage: `url(${chirtae})`}} className="rounded-[1.578vw] border-[0.053vw] border-[#000] bg-[#F6F6F6] w-[15.518vw] h-[18.609vw] bg-cover bg-center bg-no-repeat">
 
                         </div>
                     </div>
